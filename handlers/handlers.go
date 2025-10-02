3687efc5c631bcc6cfc45f2dbd75570b1a94d12f
Guillermo Fernández <bengallme@gmail.com>
2025-10-02 15:56:56 -0600

    added handlers package

---
+++ handlers/handlers.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"encoding/json"
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func EchoHandlerFunc(n *maelstrom.Node) func(maelstrom.Message) error {
+
+	return func(msg maelstrom.Message) error {
+		var body map[string]any
+
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "echo_ok"
+
+		return n.Reply(msg, body)
+
+	}
+}
--- main.go
+++ main.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+	"gossip-glomers/handlers"
 )
 
 func main() {
 	n := maelstrom.NewNode()
 
-	n.Handle("echo", func(msg maelstrom.Message) error {
-		var body map[string]any
-
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		body["type"] = "echo_ok"
-
-		return n.Reply(msg, body)
-
-	})
+	n.Handle("echo", handlers.EchoHandlerFunc(n))
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
