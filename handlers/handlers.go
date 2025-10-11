ae7bb9b332fe4ad0cf3efa9784090c42410b3a37
Guillermo Fernandez <bengallme@gmail.com>
2025-10-10 21:22:59 -0600

    broadcast challenge #3a is working now

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -38,3 +38,55 @@ func GenerateHandlerFunc(n *maelstrom.Node) func(maelstrom.Message) error {
 
 	}
 }
+
+type BroadcastBody struct {
+	Message int `json:"message"`
+}
+
+type BroadcastReply struct {
+	Type string `json:"type"`
+}
+
+func BroadcastHandlerFunc(n *maelstrom.Node, receivedMessages *[]int) func(maelstrom.Message) error {
+	return func(msg maelstrom.Message) error {
+		var bb BroadcastBody
+
+		if err := json.Unmarshal(msg.Body, &bb); err != nil {
+			return err
+		}
+
+		*receivedMessages = append(*receivedMessages, bb.Message)
+
+		replyBody := BroadcastReply{Type: "broadcast_ok"}
+
+		return n.Reply(msg, replyBody)
+
+	}
+}
+
+type ReadReply struct {
+	Type     string `json:"type"`
+	Messages []int  `json:"messages"`
+}
+
+func ReadHandlerFunc(n *maelstrom.Node, receivedMessages *[]int) func(maelstrom.Message) error {
+	return func(msg maelstrom.Message) error {
+
+		replyBody := ReadReply{Type: "read_ok", Messages: *receivedMessages}
+
+		return n.Reply(msg, replyBody)
+	}
+}
+
+type TopologyReply struct {
+	Type string `json:"type"`
+}
+
+func TopologyHandlerFunc(n *maelstrom.Node) func(maelstrom.Message) error {
+	return func(msg maelstrom.Message) error {
+
+		replyBody := TopologyReply{Type: "topology_ok"}
+
+		return n.Reply(msg, replyBody)
+	}
+}
--- main.go
+++ main.go
@@ -10,8 +10,13 @@ import (
 func main() {
 	n := maelstrom.NewNode()
 
+	var receivedMessages []int
+
 	n.Handle("echo", handlers.EchoHandlerFunc(n))
 	n.Handle("generate", handlers.GenerateHandlerFunc(n))
+	n.Handle("broadcast", handlers.BroadcastHandlerFunc(n, &receivedMessages))
+	n.Handle("read", handlers.ReadHandlerFunc(n, &receivedMessages))
+	n.Handle("topology", handlers.TopologyHandlerFunc(n))
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
